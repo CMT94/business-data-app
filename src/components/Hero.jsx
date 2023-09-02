df2439031bd1b9fdbd48c0c0c11fbc40bbef32da
Clément MGT <cmt94.dev@gmail.com>
2023-09-02 10:06:36 +0200

    Hero section + install react-simple-typewriter

---
+++ src/components/Hero.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import { Typewriter } from "react-simple-typewriter";
+const Hero = () => {
+  return (
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <p className="text-[#00df9a] font-bold p-2">
+          GROWING WITH DATA ANALYTICS
+        </p>
+        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
+          Grow with data.
+        </h1>
+        <div className="flex justify-center items-center">
+          <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">
+            Fast, flexible financing for
+          </p>
+          <p className="md:text-5xl sm:text-4xl text-xl text-[#6d6d6d] font-bold md:pl-4 pl-3">
+            <Typewriter
+              words={["BtoB", "BtoC", "SaaS"]}
+              typeSpeed={120}
+              deleteSpeed={100}
+              delaySpeed={2000}
+              loop
+              cursor
+            />
+          </p>
+        </div>
+        <p className="md:text-2xl text-xl font-bold text-gray-500">
+          Monitor your data analytics to increase revenue for BtoB, BtoC & SaaS
+          platforms.
+        </p>
+        <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 text-black hover:bg-[#02c98c]">
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
       <h1 className="w-full text-3xl font-bold text-[#00df9a]">DATABIZ.</h1>
-      <ul className="hidden md:flex capitalize">
+      <ul className="capitalize hidden md:flex">
         {navLinks.map((link) => (
           <li key={link.key} className="p-4">
             {link.key}
@@ -39,14 +39,14 @@ const Navbar = () => {
 
       {/* MOBILE NAV - START */}
       <div onClick={toggleNavMenu} className="block md:hidden">
-        {!navMenu ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        {navMenu ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
 
       <div
         className={
-          !navMenu
+          navMenu
             ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
-            : "fixed left-[-100%]"
+            : "fixed top-0 left-[-100%]"
         }
       >
         <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">
